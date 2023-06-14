ae6c10cfe893663c0f19a4a152fa744a502546e1
JhumerA <jhumerapus@gmail.com>
2023-06-15 02:06:59 +0800

    updated

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: "static",
+  server: {
+    host: process.env.APP_URL,
+  },
   head: {
     title: "eDokumento",
     htmlAttrs: {
